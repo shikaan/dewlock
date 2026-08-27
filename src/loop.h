8d937372ce48798053aeb268a8cf2fbbab395eda
Manuel Spagnolo <manuel.spagnolo@proton.me>
2026-08-27 13:40:49 +0200

    remove noisy comments

--- src/loop.h
+++ src/loop.h
@@ -1,51 +1,15 @@
 #pragma once
 #include <stdbool.h>
 
-/**
- * This is an event loop system designed for sway clients, not sway itself.
- *
- * The loop consists of file descriptors and timers. Typically the Wayland
- * display's file descriptor will be one of the fds in the loop.
- */
-
 typedef struct loop loop_t;
 typedef struct loop_timer loop_timer_t;
 
-/**
- * Create an event loop.
- */
 loop_t *loop_create(void);
-
-/**
- * Destroy the event loop (eg. on program termination).
- */
 void loop_destroy(loop_t *loop);
-
-/**
- * Poll the event loop. This will block until one of the fds has data.
- */
 void loop_poll(loop_t *loop);
-
-/**
- * Add a file descriptor to the loop.
- */
 void loop_add_fd(loop_t *loop, int fd, short mask,
                  void (*func)(int fd, short mask, void *data), void *data);
-
-/**
- * Add a timer to the loop.
- *
- * When the timer expires, the timer will be removed from the loop and freed.
- */
 loop_timer_t *loop_add_timer(loop_t *loop, int ms,
                              void (*callback)(void *data), void *data);
-
-/**
- * Remove a file descriptor from the loop.
- */
 bool loop_remove_fd(loop_t *loop, int fd);
-
-/**
- * Remove a timer from the loop.
- */
 bool loop_remove_timer(loop_t *loop, loop_timer_t *timer);
--- src/unicode.h
+++ src/unicode.h
@@ -8,30 +8,8 @@
 
 #define UTF8_INVALID 0x80
 
-/**
- * Gets the size in bytes of the last utf8 character in a NULL terminated string
- * This function does not validate that the buffer contains correct utf8 data;
- * it merely looks for the first byte that correctly denotes the beginning of a
- * utf8 character.
- */
 int utf8_last_size(const char *str);
-
-/**
- * Grabs the next UTF-8 character and advances the string pointer
- */
 uint32_t utf8_decode(const char **str);
-
-/**
- * Encodes a character as UTF-8 and returns the length of that character.
- */
 size_t utf8_encode(char *str, uint32_t ch);
-
-/**
- * Returns the size of the next UTF-8 character
- */
 int utf8_size(const char *str);
-
-/**
- * Returns the size of a UTF-8 character
- */
 size_t utf8_chsize(uint32_t ch);
